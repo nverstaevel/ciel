abab17c6c579f827c57a8f4da4c8d9f41ba24346
Jordan Levy <jordan.levy310@gmail.com>
2024-12-13 11:31:42 +0100

    add n_class classification and wttschereck xp

--- torch_mas/sequential/internal_model/__init__.py
+++ torch_mas/sequential/internal_model/__init__.py
@@ -1,5 +1,7 @@
 from .model_interface import InternalModelInterface
 from .linear_reg import LinearWithMemory
 from .svm import SVM
+from .n_class import NClass
 
-__all__ = ["InternalModelInterface", "LinearWithMemory","SVM"]
+
+__all__ = ["InternalModelInterface", "LinearWithMemory", "SVM", "NClass"]
