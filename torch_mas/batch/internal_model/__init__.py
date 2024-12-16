b90bdb8c16d91761e92304581bdad4fbfe49334e
Jordan Levy <jordan.levy310@gmail.com>
2024-12-16 10:30:04 +0100

    NClass batch

--- torch_mas/batch/internal_model/__init__.py
+++ torch_mas/batch/internal_model/__init__.py
@@ -2,5 +2,6 @@ from .model_interface import InternalModelInterface
 from .linear_reg import LinearWithMemory
 from .linear_reg_sgd import LinearSGD
 from .svm import SVM
+from .n_class import NClass
 
-__all__ = ["InternalModelInterface", "LinearWithMemory", "LinearSGD", "SVM"]
+__all__ = ["InternalModelInterface", "LinearWithMemory", "LinearSGD", "SVM", "NClass"]
