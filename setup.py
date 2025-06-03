a3a1395f2bcf48c32ff01f571620d561c291a532
Paul Saves <paul.saves@free.fr>
2025-06-03 09:47:54 +0200

    improve setup

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
-from setuptools import setup, find_packages
+from setuptools import Extension, setup
 
-setup(name="torch_mas", version="0.1", packages=find_packages())
+metadata = dict(
+    name="ciel",
+    version=0.1,
+    description="CIEL (Contextual Interactive Ensemble Learning)",
+    author="Nicolas Vestaevel et al.",
+    author_email="nicolas.verstaevel@irit.fr",
+    license="MIT",
+    packages=[
+        "ciel",
+        "ciel.torch_mas",
+    ],
+    install_requires=[
+        "packaging",
+        "scikit-learn",
+        "torch",
+        "scipy",
+    ],
+    python_requires=">=3.9",
+    zip_safe=False,
+    url="https://github.com/nverstaevel/ciel,  # use the URL to the github repo
+    download_url="https://github.com/nverstaevel/ciel/releases",
+)
+
+setup(**metadata)
